1ecc4c890da02d62928cc4b3cc7178bd6b26a4e4
Finskou <lelarhaz@gmail.com>
2020-02-10 20:58:45 +0100

    del value of last username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,6 +25,11 @@ class User implements UserInterface
     private $email;
 
     /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
      * @ORM\Column(type="json")
      */
     private $roles = [];
@@ -60,6 +65,12 @@ class User implements UserInterface
     public function getUsername(): string
     {
         return (string) $this->email;
+    }
+
+    public function setUsername(string $username): self 
+    {
+        $this->username = $username;
+        return $this;
     }
 
     /**
